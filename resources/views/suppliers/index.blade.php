14a170cba3e68da8717b4c626506bd480b4efadc
mohamed-azzouz1 <339078@student.mboutrecht.nl>
2025-06-27 10:10:01 +0200

    overzicht klaar

--- resources/views/suppliers/index.blade.php
+++ resources/views/suppliers/index.blade.php
@@ -163,8 +163,11 @@
                             </tr>
                         @empty
                             <tr>
-                                <td colspan="7" class="px-6 py-4 text-center text-gray-500">
-                                    Geen leveranciers gevonden
+                                <td colspan="7" class="px-6 py-8 text-center bg-red-50">
+                                    <div class="flex flex-col items-center p-4 border border-red-200 rounded-lg bg-red-100">
+                                        <p class="text-red-700 font-bold text-lg">Geen leveranciers gevonden</p>
+                                        <p class="text-red-600 mt-1">Er zijn momenteel geen actieve leveranciers in het systeem</p>
+                                    </div>
                                 </td>
                             </tr>
                         @endforelse
@@ -223,7 +226,6 @@
                                     <a href="{{ $suppliers->previousPageUrl() }}" class="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
                                         <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                                             <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
-                                        </svg>
                                     </a>
                                 @endif
 
